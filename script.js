e6d650defd6453ce719c66df0f5d8d7b2c146eaf
jbpaterson <jack.paterson@live.com>
2026-02-25 14:51:11 +1000

    Update download links to 0.9.12 DMG

--- script.js
+++ script.js
@@ -231,7 +231,7 @@ if (buyLink) {
 }
 
 const defaultReleaseDownloadUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.11/TAPE-16-v0.9.11-macOS.dmg";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.12/TAPE-16-v0.9.12-macOS.dmg";
 const releaseDownloadUrl = configUrl(config.releaseDownloadUrl) || defaultReleaseDownloadUrl;
 
 if (demoLink) {
