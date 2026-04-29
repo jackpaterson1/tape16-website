f15ccf2dd7fef467ee9d97003f75497988c74956
jbpaterson <jack.paterson@live.com>
2026-04-30 08:26:06 +1000

    Relink downloads to 0.9.220 release

--- script.js
+++ script.js
@@ -261,11 +261,11 @@ if (buyLink) {
 }
 
 const pinnedReleaseDownloadUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.212/TAPE-16-v0.9.212-macOS.dmg";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.220/TAPE-16-v0.9.220-macOS.dmg";
 const pinnedWindowsDownloadUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.212/TAPE16-Windows-Release-0.9.212.zip";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.220/TAPE16-Windows-Release-0.9.220.zip";
 const pinnedGithubReleaseUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/tag/0.9.212";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/tag/0.9.220";
 const releaseDownloadUrl = configUrl(config.releaseDownloadUrl) || pinnedReleaseDownloadUrl;
 const windowsDownloadUrl = configUrl(config.windowsDownloadUrl) || pinnedWindowsDownloadUrl;
 const githubReleaseUrl = configUrl(config.githubReleaseUrl) || pinnedGithubReleaseUrl;
