9a62f8fdfeb7fa23d07c4490173b77f8309621c3
jbpaterson <jack.paterson@live.com>
2026-05-29 14:56:19 +1000

    Update website downloads to 0.9.261

--- script.js
+++ script.js
@@ -261,11 +261,11 @@ if (buyLink) {
 }
 
 const pinnedReleaseDownloadUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.231/TAPE-16-v0.9.231-macOS.dmg";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.261/TAPE-16-v0.9.261-macOS.dmg";
 const pinnedWindowsDownloadUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.231/TAPE16-Windows-Release-0.9.231.zip";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.261/TAPE-16-v0.9.261-Windows-Setup.zip";
 const pinnedGithubReleaseUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/tag/0.9.231";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/tag/0.9.261";
 const releaseDownloadUrl = configUrl(config.releaseDownloadUrl) || pinnedReleaseDownloadUrl;
 const windowsDownloadUrl = configUrl(config.windowsDownloadUrl) || pinnedWindowsDownloadUrl;
 const githubReleaseUrl = configUrl(config.githubReleaseUrl) || pinnedGithubReleaseUrl;
--- tape16/script.js
+++ tape16/script.js
@@ -506,11 +506,11 @@ if (buyLink) {
 }
 
 const pinnedReleaseDownloadUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.251/TAPE-16-v0.9.251-macOS.dmg";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.261/TAPE-16-v0.9.261-macOS.dmg";
 const pinnedWindowsDownloadUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.252-WindowsPatch/TAPE-16-v0.9.252-Windows-Setup.exe";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.261/TAPE-16-v0.9.261-Windows-Setup.zip";
 const pinnedGithubReleaseUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/tag/0.9.252-WindowsPatch";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/tag/0.9.261";
 const releaseDownloadUrl = configUrl(config.releaseDownloadUrl) || pinnedReleaseDownloadUrl;
 const windowsDownloadUrl = configUrl(config.windowsDownloadUrl) || pinnedWindowsDownloadUrl;
 const githubReleaseUrl = configUrl(config.githubReleaseUrl) || pinnedGithubReleaseUrl;
