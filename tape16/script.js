91f605eea5d941ed70a65b00c010b6c9ae44e1b2
Jack Paterson <jack@Jacks-MacBook-Air.local>
2026-06-21 19:13:16 +1000

    Update website downloads to 0.9.292

--- tape16/script.js
+++ tape16/script.js
@@ -847,11 +847,11 @@ async function configurePayPalCheckout() {
 configurePayPalCheckout();
 
 const pinnedReleaseDownloadUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.291/TAPE-16-v0.9.291-macOS.dmg";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.292/TAPE-16-v0.9.292-macOS.dmg";
 const pinnedWindowsDownloadUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.291/TAPE-16-v0.9.291-Windows-Setup.zip";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.292/TAPE-16-v0.9.292-Windows-Setup.zip";
 const pinnedGithubReleaseUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/tag/0.9.291";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/tag/0.9.292";
 const releaseDownloadUrl = configUrl(config.releaseDownloadUrl) || pinnedReleaseDownloadUrl;
 const windowsDownloadUrl = configUrl(config.windowsDownloadUrl) || pinnedWindowsDownloadUrl;
 const githubReleaseUrl = configUrl(config.githubReleaseUrl) || pinnedGithubReleaseUrl;
