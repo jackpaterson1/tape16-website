5899a2e18b617bc49537fa9260a989009ffad025
Jack Paterson <jack@Jacks-MacBook-Air.local>
2026-07-17 15:21:56 +1000

    Update downloads to 0.9.320

--- script.js
+++ script.js
@@ -261,11 +261,11 @@ if (buyLink) {
 }
 
 const pinnedReleaseDownloadUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.311/TAPE-16-v0.9.311-macOS.dmg";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.320/TAPE-16-v0.9.320-macOS.dmg";
 const pinnedWindowsDownloadUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.311/TAPE-16-v0.9.311-Windows-Setup.zip";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.320/TAPE-16-v0.9.320-Windows-Setup.zip";
 const pinnedGithubReleaseUrl =
-  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/tag/0.9.311";
+  "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/tag/0.9.320";
 const releaseDownloadUrl = configUrl(config.releaseDownloadUrl) || pinnedReleaseDownloadUrl;
 const windowsDownloadUrl = configUrl(config.windowsDownloadUrl) || pinnedWindowsDownloadUrl;
 const githubReleaseUrl = configUrl(config.githubReleaseUrl) || pinnedGithubReleaseUrl;
