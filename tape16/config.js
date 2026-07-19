338ee4b3dc75ae165c107c28b20c0bc5dba4db87
Jack Paterson <jack@Jacks-MacBook-Air.local>
2026-07-19 22:49:38 +1000

    Update Linux download to 0.9.320

--- tape16/config.js
+++ tape16/config.js
@@ -1,7 +1,7 @@
 window.TAPE16_SITE_CONFIG = {
   releaseDownloadUrl: "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.320/TAPE-16-v0.9.320-macOS.dmg",
   windowsDownloadUrl: "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.320/TAPE-16-v0.9.320-Windows-Setup.zip",
-  linuxDownloadUrl: "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.310/TAPE-16-v0.9.310-Linux-Release.zip",
+  linuxDownloadUrl: "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/download/0.9.320/TAPE-16-v0.9.320-Linux-Release.zip",
   githubReleaseUrl: "https://github.com/jackpaterson1/TAPE-16-Public-Releases/releases/tag/0.9.320",
   // Replace this with your live checkout URL (Stripe / Gumroad / Paddle / etc).
   buyNowUrl: "https://buy.stripe.com/28E9AT4Tz7e50D6bqm1ck02",
